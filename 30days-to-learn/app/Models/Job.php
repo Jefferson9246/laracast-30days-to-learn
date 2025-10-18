4848929a1485c7916d8899697eb4e2dd0b27d23c
Jefferson Sousa <jeffersonrodrigomoreira@gmail.com>
2025-10-18 18:58:14 -0400

    getting done with the job model

--- 30days-to-learn/app/Models/Job.php
+++ 30days-to-learn/app/Models/Job.php
@@ -5,36 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Job
+class Job extends Model
 {
-
-    public static function all(): array
-    {
-        return [
-            [
-            'id' => 0,
-            'title' => 'architect',
-            'salary' => 2000,
-            ],
-            [
-            'id' => 1,
-            'title' => 'developer',
-            'salary' => 3000,
-            ],
-            [
-            'id' => 2,
-            'title' => 'designer',
-            'salary' => 2500,
-            ],
-        ];
-    }
-
-    public static function find($id): array
-    {
-        $job = Arr::first(self::all(), fn ($job) => $job['id'] == $id);
-        if (!$job) {
-            abort(404);
-        }
-        return $job;
-    }
+    protected $table = 'job_listings';
+    protected $fillable = [
+        'title',
+        'salary',
+    ];
 }
