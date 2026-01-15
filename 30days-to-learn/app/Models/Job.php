ed1875d023132aac1a7862fda1abdaf4b45021cd
Jefferson Sousa <jeffersonrodrigomoreira@gmail.com>
2026-01-15 10:17:30 -0400

    model tag, trabalhando com pivot

--- 30days-to-learn/app/Models/Job.php
+++ 30days-to-learn/app/Models/Job.php
@@ -21,4 +21,8 @@ class Job extends Model
     {
         return $this->belongsTo(Employer::class);
     }
+
+    public function tags(){
+        return $this->belongsToMany(Tag::class, foreignPivotKey: 'job_linsting_id');
+    }
 }
